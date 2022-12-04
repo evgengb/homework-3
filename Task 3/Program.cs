c9721cafb79c0e8e1eef371f68e7f283a2ef6276
Evgen <beu08@list.ru>
2022-12-04 14:20:28 +0600

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+Console.Write($"{message} >"); 
+return Convert.ToInt32(Console.ReadLine()); 
+}
+
+int number = Prompt("Значение =>");
+int start = 1;
+int mod = number; 
+
+if(mod<0)
+{
+    mod= -mod;
+}
+
+while (start <= mod) 
+{
+    int square = start*start*start;
+    System.Console.WriteLine(square);
+    start++;
+}
